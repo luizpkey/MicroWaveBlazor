7b5c10210d42a5b08c200dd40226fa64db89ab84
luizpkey <tecpdu@gmail.com>
2024-12-14 10:49:30 -0300

    Implementado a parte visual do Microondas, funcional, porém tem que corrigir a imagem de dentro do microondas as configurações de time pré programado

---
+++ MicroWave/Components/Pages/Microwave.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp1.Components.Pages;
+
+public class Microwave_razor : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
